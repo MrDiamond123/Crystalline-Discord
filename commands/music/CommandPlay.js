13f4a88ce1742351a24beb509c9e10ebe69425ba
MrDiamond123 <kurtisiscool123@gmail.com>
2021-02-16 01:26:40 -0500

    ADD THE MEMBERNAME

--- commands/music/CommandPlay.js
+++ commands/music/CommandPlay.js
@@ -4,6 +4,7 @@ module.exports =
         constructor(client) {
             super (client, {
                 name: 'play',
+                memberName: 'play',
                 aliases: ['themusicgoboomboom'],
                 group: 'music',
                 description: 'it play music',
